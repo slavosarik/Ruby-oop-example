06fb9154a47e0cd9d1ad081d6c1e2bfb8b39f490
slavosarik <slavosarik@gmail.com>
2014-07-17 11:00:50 +0200

    addedd controller fucntions

--- permissions_spec.rb
+++ permissions_spec.rb
@@ -1,2 +1,51 @@
 require 'rspec'
 require 'nokogiri'
+
+class User
+
+end
+
+class Account
+
+end
+
+class PermissionsController # < ApplicationController
+  def show # GET /permissions/:id
+    p = Permission.find(params[:id])
+    current_user
+  end
+
+  def index # GET /permissions
+    #zobrazenie zoznamu
+    # ak prihlaseny user ma pravo vidiet konto tak to zobrazim inak error
+    unless current_user.can_view?(current_account)
+      redirect_to_root_path
+    end
+
+  end
+
+  def create
+
+  end
+
+  def current_user
+    User.find()
+  end
+
+  def current_account
+    Account.find(params[:account_id])
+  end
+
+end
+
+RSpec.describe Test do
+  it 'permission_test' do
+
+    #vytvorit fake pouzivatela
+    #vytvorit ho s accountom
+    #zistit prava
+    #overit ak ma vytvoreny account, ta
+
+  end
+
+end
